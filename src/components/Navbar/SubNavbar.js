b0b74e2e438ccaafd5cc1f06a85f3870231f7356
ILearnEducation-Tri <ilearncomputereducation@gmail.com>
2025-02-12 16:03:33 +0530

    Fixing Hosting Issue 4

--- src/components/Navbar/SubNavbar.js
+++ src/components/Navbar/SubNavbar.js
@@ -7,12 +7,10 @@ export const SubNavbar = () => {
     <header id="myHeader">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container d-flex justify-content-between align-items-center">
-            {/* Left: Company Icon */}
-            <Link className="navbar-brand" to="/">
-  <img src="/logo192.png" className="w-50 w-sm-25 w-xs-10" alt="ILearn" />
-</Link>
-
-
+          {/* Left: Company Icon */}
+          <Link className="navbar-brand" to="/">
+            <img src="/logo192.png" className="w-50 w-sm-25 w-xs-10" alt="ILearn" />
+          </Link>
 
           {/* Center: Company Name */}
           <h1 className="m-0  d-none d-sm-block transform-head">
@@ -22,11 +20,9 @@ export const SubNavbar = () => {
 
           {/* Right: Home Icon with Text */}
           <Link className="nav-link d-flex flex-column align-items-center g-0" to="/">
-  <i className="fas fa-home text-secondary fa-3x fa-sm-2x"></i>
-  <p className="m-0 text-secondary small-screen-text">Home</p>
-</Link>
-
-
+            <i className="fas fa-home text-secondary fa-3x fa-sm-2x"></i>
+            <p className="m-0 text-secondary small-screen-text">Home</p>
+          </Link>
         </div>
       </nav>
     </header>
--- src/components/SoftwareList/Software.js
+++ src/components/SoftwareList/Software.js
@@ -23,40 +23,7 @@ function SoftwareList() {
             <li key={index} className="p-2">
                 <img src={`/assets/icons/${item}.png`} alt={item} className="icon" />
             </li>
-            ))}            
-            {/* <li className="p-2">
-                <img src="/assets/icons/Illustrator.png" alt="Illustrator" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/InDesign.png" alt="InDesign" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/Lightroom.png" alt="Lightroom" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/CharacterAnimator.png" alt="Character Animator" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/Nuke.png" alt="Nuke" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/Maya.png" alt="Maya" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/Blender.png" alt="Blender" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/PremierePro.png" alt="Premiere Pro" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/Audition.png" alt="Audition" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/AfterEffects.png" alt="After Effects" className="icon"/>
-            </li>
-            <li className="p-2">
-                <img src="/assets/icons/4D.png" alt="4D" className="icon"/>
-            </li> */}
+            ))}
         </ul>
     </section>
   )
